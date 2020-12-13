637adde55a5780a36ee60a336e1caa3b066caf85
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-13 22:30:19 +0800

    added Navbar Links to app

--- components/Container.js
+++ components/Container.js
@@ -1,19 +1,58 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
-import Link from 'next/link';
+import NextLink from 'next/link';
+import styled from 'styled-components'
 
-export default function Container({ children }) {
+export default function Body({ children }) {
 
     const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
     useEffect(() => setMounted(true), []);
 
+    const Container = styled.div`
+    max-width: 54rem;
+    margin:0 auto;
+    @media (min-width: 768px) {
+    }
+    `;
+    const NavMenu = styled.a`
+    display: flex;
+    justify-content: flex-end;
+    padding: 2rem;
+    `
+    const NavbarLinks = styled.a`
+    font-size: 1.25rem;
+    color: gray;
+    padding: 0.25rem;
+    @media (min-width: 640px) {
+        padding: 1rem;
+    }
+    `
+    const BodyMain = styled.main`
+    padding-left: 2rem;
+    padding-right: 2rem;
+    `
+
     return (
-        <div>
-            <p>Testing Children from Container</p>
-            <main>
+
+        <Container maxW="10%" centerContent={true}>
+            <nav>
+                {/* TODO */}
+                <NavMenu>
+                    <NextLink href="/blog">
+                        <NavbarLinks>Blog</NavbarLinks>
+                    </NextLink>
+                    <NextLink href="/about">
+                        <NavbarLinks>About</NavbarLinks>
+                    </NextLink>
+                    <NextLink href="/">
+                        <NavbarLinks>Home</NavbarLinks>
+                    </NextLink>
+                </NavMenu>
+            </nav>
+            <BodyMain>
                 {children}
-            </main>
-        </div>
+            </BodyMain>
+        </Container>
     )
 }
--- components/Title.js
+++ components/Title.js
@@ -1,28 +1,42 @@
-import { Text } from '@chakra-ui/react'
+import { Text } from "@chakra-ui/react"
 import Typed from 'react-typed'
 import styled from 'styled-components'
 
 
 export default function Title({ contents }) {
 
-    const TitleTyped = styled.div`
+    const TitleContainer = styled.div`
     display:flex;
     flex-direction:column;
     justify-content: center;
     text-align: center;
-    font-size: 3em;
+    margin-bottom: 2rem;
+    @media (min-width: 768px) {
+    }
     `
-
+    const TitleText = styled(Text)`
+    font-size: 2.5rem;
+    @media (min-width: 768px) {
+        font-size: 4rem;
+    }
+    `
+    const TitleGIF = styled(Typed)`
+    font-size: 1.35rem;
+    @media (min-width: 768px) {
+        font-size: 3rem;
+    }
+    `
+    
     return (
-        <TitleTyped>
-            <Text fontSize="5xl">I'm Min Khant Kyaw</Text>
-            <Typed
+        <TitleContainer>
+            <TitleText>Hi! I'm Min</TitleText>
+            <TitleGIF
                 strings={contents}
                 typeSpeed={40}
                 backSpeed={50}
                 smartBackspace={true}
                 loop>
-            </Typed>
-        </TitleTyped>
+            </TitleGIF>
+        </TitleContainer>
     )
 }
