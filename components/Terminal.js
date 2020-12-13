3d00dcf2aab53e85bb48b0c14e2db6f6b15b4711
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-13 22:31:05 +0800

    added react-bootstrap for existing terminal

--- components/Terminal.js
+++ components/Terminal.js
@@ -1,5 +1,48 @@
+import { TableRow } from '@material-ui/core'
+import styled from 'styled-components'
+import { Jumbotron, Container, Row } from 'react-bootstrap';
 
-export default function Terminal(props) {
+export default function Terminal() {
+
+    const TerminalDiv = styled(Jumbotron)`
+    background: transparent;
+    padding: 1em 0;
+    max-width: 750px;
+    margin: 0 auto;
+    width:100%;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    flex-direction: column;
+    `
+    const TerminalBar = styled.div`
+    align-items: center;
+    height: 2em;
+    background-color: #E4E3E5;
+    max-width: 1000px;
+    border-radius: 10px 10px 0 0;
+    `
+    const TerminalButtons = styled.div`
+    width: 15px;
+    height: 15px;
+    background-color: #33c948;
+    border-radius: 50%;
+    margin-left: 0.5em;
+    `
+    const TerminalBody = styled.div`
+    justify-content: center;
+    background: #404040;
+    color: #fff;
+    `
+    const TerminalRow = styled(TableRow)`
+    margin: 0;
+    padding: 1.25em 1em;
+    `
+    const TerminalLink = styled.a`
+    color: white;
+    font-size: 1.1em;
+    `
+
     const info = [{
         title: "UD.fullName",
         value: "Min Khant Kyaw"
@@ -11,7 +54,7 @@ export default function Terminal(props) {
     }, {
         title: "UD.resume",
         value: "minkhantkyaw.pdf",
-        link: Resume
+        link: "Resume"
     }, {
         title: "UD.education",
         value: "Diploma in Information Technology (SP)",
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import Title from '../components/Title'
 import { Button, Link } from "@chakra-ui/react"
-import Container from '../components/Container';
+import Body from '../components/Container';
+import Terminal from '../components/Terminal';
 
-const BlogButton = () => (
-  <div>
-    <Button variant="outlined" color="primary" href="#outlined-buttons">
-      <Link>Know thy author!</Link>
-    </Button>
-  </div>
-);
 
 const HomeContent = () => (
-  <Container>
-    <Title contents={['I\'m <strong>a software devloper', 'I\'m <strong>a web and mobile app developer', 'I\'m <strong>an AI/ML enthusiast']} />
-    <BlogButton />
-  </Container>
+  <Body>
+    <Title contents={['I\'m <strong>a web developer.', 'I\'m <strong>a mobile app developer.', 'I\'m <strong>an AI/ML enthusiast.']} />
+    <p>I'm a developer and a wrtier. I am a software developer working at Arrowcrest Technologies. This is my personal profile - everything about me can be found here.</p>
+    <Terminal />
+  </Body>
 );
 
 export default HomeContent;
