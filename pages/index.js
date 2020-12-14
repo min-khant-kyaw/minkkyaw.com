6b83c20740378f09e665bb111b110373268c8e8e
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-14 22:47:15 +0800

    styling improved

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,10 @@ import Terminal from '../components/Terminal';
 const HomeContent = () => (
   <Body>
     <Title contents={['I\'m <strong>a web developer.', 'I\'m <strong>a mobile app developer.', 'I\'m <strong>an AI/ML enthusiast.']} />
-    <p>I'm a developer and a wrtier. I am a software developer working at Arrowcrest Technologies. This is my personal profile - everything about me can be found here.</p>
+    <p>
+      I'm a developer and a wrtier. I am a software developer working at Arrowcrest Technologies. 
+      This is my personal profile - everything about me can be found here.
+    </p>
     <Terminal />
   </Body>
 );
