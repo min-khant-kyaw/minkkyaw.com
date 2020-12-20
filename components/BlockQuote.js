3e6166b2d7260aae88556b15de9f14699ab146fc
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-20 22:06:31 +0800

    changed css for BlockQuote

--- components/BlockQuote.js
+++ components/BlockQuote.js
@@ -5,6 +5,7 @@ const BlockQuote = ({ children }) => {
   const QuoteContainer = styled(Container)`
     border-left: 5px solid red;
     margin-left: 1em;
+    padding-left: 1em;
   `;
 
   return <QuoteContainer>{children}</QuoteContainer>;
