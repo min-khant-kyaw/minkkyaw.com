1328f8e367bad4d2607c8bf3dfa13afdf0a89b84
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-22 12:14:19 +0800

    added next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,31 @@
+const title = 'Min Khant Kyaw – Developer, Writer';
+const description =
+  'Front-end developer, Machine Learning enthusiast, Javascript junkie.';
+
+const SEO = {
+  title,
+  description,
+  canonical: 'https://minkkyaw.com',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://minkkyaw.com',
+    title,
+    description,
+    images: [
+      {
+        url: 'https://leerob.io/static/images/banner.jpg',
+        alt: title,
+        width: 1280,
+        height: 720
+      }
+    ]
+  },
+  twitter: {
+    handle: '@therealUD',
+    site: '@therealUD',
+    cardType: 'summary_large_image'
+  }
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import App from "next/app";
 import Head from "next/head";
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { DefaultSEO } from 'next-seo';
 import "../css/main.css";
+
 
 const theme = {
   colors: {
@@ -18,12 +20,32 @@ export default class MyApp extends App {
     return (
       <ThemeProvider theme={theme}>
         <Head>
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicons/site.webmanifest" rel="manifest" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&family=Lato&display=swap"
             rel="stylesheet"
           />
-          <title>Min Khant Kyaw - Developer, Writer</title>
+          <link
+            href="/static/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/static/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/static/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <Component {...pageProps} />
       </ThemeProvider>
