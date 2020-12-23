97fcfbea113544c08f7c911e2d071749d33a7307
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-23 13:59:26 +0800

    inproved mdx code styling

--- components/mdx-styles/MultiCode.js
+++ components/mdx-styles/MultiCode.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 
 const CodeContainer = styled(Box)`
   padding: 0.2rem 0.4rem;
-  margin: 0;
+  margin: 0.25rem 0;
   font-size: 85%;
   background-color: #1b1f230d;
   border-radius: 6px;
+  @media (min-width: 768px) {
+    margin: 0.5rem 0;
+  }
 `;
 
 const MultiCode = ({ children }) => {
