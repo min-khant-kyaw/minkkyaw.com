2ac57d5de23bac1039c755ed65bae4d41e5e9fde
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-23 09:46:31 +0800

    moved components

--- components/Title.js
+++ components/Title.js
@@ -2,31 +2,29 @@ import { Text } from "@chakra-ui/react"
 import Typed from 'react-typed'
 import styled from 'styled-components'
 
+const TitleContainer = styled.div`
+display:flex;
+flex-direction:column;
+justify-content: center;
+text-align: center;
+margin-bottom: 2rem;
+@media (min-width: 768px) {
+}
+`
+const TitleText = styled(Text)`
+font-size: 2.5rem;
+@media (min-width: 768px) {
+    font-size: 4rem;
+}
+`
+const TitleGIF = styled(Typed)`
+font-size: 1.35rem;
+@media (min-width: 768px) {
+    font-size: 3rem;
+}
+`
 
 export default function Title({ contents }) {
-
-    const TitleContainer = styled.div`
-    display:flex;
-    flex-direction:column;
-    justify-content: center;
-    text-align: center;
-    margin-bottom: 2rem;
-    @media (min-width: 768px) {
-    }
-    `
-    const TitleText = styled(Text)`
-    font-size: 2.5rem;
-    @media (min-width: 768px) {
-        font-size: 4rem;
-    }
-    `
-    const TitleGIF = styled(Typed)`
-    font-size: 1.35rem;
-    @media (min-width: 768px) {
-        font-size: 3rem;
-    }
-    `
-    
     return (
         <TitleContainer>
             <TitleText>Hi! I'm Min</TitleText>
