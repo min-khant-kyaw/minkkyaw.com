d6334869b3a1d484a52e1b556ff385d57d16fcf9
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-24 10:25:07 +0800

    fixed BlogTags styling

--- components/BlogTags.js
+++ components/BlogTags.js
@@ -1,17 +1,30 @@
 import { Avatar, Badge, Flex, Text, Box } from "@chakra-ui/react";
+import styled from 'styled-components';
+
+const FlexBox = styled(Box)`
+display: flex;
+flex-direction: column;
+`
+const Name = styled(Text)`
+font-size: 1.25rem;
+font-weight: bold;
+`
+const Date = styled(Text)`
+color: #303030;
+`
+const Read = styled(Text)`
+color: #404040;
+`
 
 const BlogTags = ({ author, date, read }) => {
   return (
     <Flex>
       <Box ml="3">
-        <Text fontWeight="bold">{author}</Text>
-        <Text fontSize="sm">
-          <Badge colorScheme="green">
-            {date}
-          </Badge>
-          {"  "}
-          {read}
-        </Text>
+        <Name fontWeight="bold">{author}</Name>
+        <Box display="flex" flexDir="column">
+          <Date fontSize="mds">{date}</Date>
+          <Read>{read}</Read>
+        </Box>
       </Box>
     </Flex>
   );
