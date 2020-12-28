b2229284ae425f28af32c312cd0882bd39e22d67
min-khant-kyaw <min.kkyaw0101@gmail.com>
2020-12-28 16:31:38 +0800

    fixed chakraUI theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,23 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ThemeProvider, theme } from "@chakra-ui/react";
 import App from "next/app";
 import React from "react";
-import { ThemeProvider } from "styled-components";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 import "../css/main.css";
 import Head from "next/head";
-
-const styling = {
-  styles: {
-    global: (props) => ({
-      a: {
-        color: props.colorMode === "dark" ? "teal.300" : "teal.500",
-      },
-      h1: {
-        marginTop: "3rem",
-        marginBottom: "1.5rem",
-      }
-    }),
-  },
-}
-
-const theme = extendTheme({styling})
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <>
-        <ChakraProvider theme={theme}>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Head>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </ThemeProvider>
     );
   }
 }
--- theme.js
+++ theme.js
@@ -1,7 +1,41 @@
 export default {
-  fonts: {
-    body: "Lato, Inter, Arial, Helvetica, sans-serif",
-    heading: "Lato, Inter, Arial, Helvetica, sans-serif",
-    mono: "Lato, Inter, Arial, Helvetica, sans-serif",
-  },
-};
+    fonts: {
+      body: "Inter, Lato, Arial, Helvetica, sans-serif",
+      heading: "Inter, Lato, Arial, Helvetica, serif",
+      mono: "Inter, Lato, Arial, Helvetica, sans-serif",
+    },
+    fontSizes: {
+      xs: "12px",
+      sm: "14px",
+      md: "16px",
+      lg: "18px",
+      xl: "20px",
+      "2xl": "24px",
+      "3xl": "28px",
+      "4xl": "36px",
+      "5xl": "48px",
+      "6xl": "64px",
+    },
+    fontWeights: {
+      normal: 400,
+      medium: 500,
+      bold: 700,
+    },
+    lineHeights: {
+      normal: "normal",
+      none: "1",
+      shorter: "1.25",
+      short: "1.375",
+      base: "1.5",
+      tall: "1.625",
+      taller: "2",
+    },
+    letterSpacings: {
+      tighter: "-0.05em",
+      tight: "-0.025em",
+      normal: "0",
+      wide: "0.025em",
+      wider: "0.05em",
+      widest: "0.1em",
+    },
+  };
